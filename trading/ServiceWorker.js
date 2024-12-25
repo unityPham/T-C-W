3694698c3bc861b795e06576cd6c4ab5ffafeb7f
unityPham <unitypham24h@gmail.com>
2024-12-25 07:24:54 +0700

    try fix

--- trading/ServiceWorker.js
+++ trading/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "SliBox-Trade Coin Bot-2024.12.25.33";
+const cacheName = "SliBox-Trade Coin Bot-2024.12.25.441";
 const contentToCache = [
     "Build/trading.loader.js",
     "Build/trading.framework.js",
